27c06fe71065d4227d456519633a953aca48cd5d
aeoank <asarkar@aeonixinnovations.com>
2024-12-22 21:24:08 +0530

    Create server.mjs

---
+++ server.mjs
@@ -0,0 +1,65 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import open from 'open';
+import wrtc from 'wrtc';  // Import the entire module as default
+
+const { RTCPeerConnection, RTCSessionDescription } = wrtc;  // Destructure the needed classes
+
+let senderStream;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/consumer", async ({ body }, res) => {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    const desc = new RTCSessionDescription(body.sdp);
+    await peer.setRemoteDescription(desc);
+    senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    const payload = {
+        sdp: peer.localDescription
+    }
+
+    res.json(payload);
+});
+
+app.post('/broadcast', async ({ body }, res) => {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    peer.ontrack = (e) => handleTrackEvent(e, peer);
+    const desc = new RTCSessionDescription(body.sdp);
+    await peer.setRemoteDescription(desc);
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+    const payload = {
+        sdp: peer.localDescription
+    }
+
+    res.json(payload);
+});
+
+function handleTrackEvent(e, peer) {
+    senderStream = e.streams[0];
+};
+
+// Start the server and open the browser window
+app.listen(5000, () => {
+    console.log(`Server started at http://localhost:5000`);
+    // Open the browser automatically
+    open(`http://localhost:5000`);
+});
