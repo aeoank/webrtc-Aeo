8c999c9851b24f353cfeb748a218506714be87b2
aeoank <asarkar@aeonixinnovations.com>
2024-12-22 21:22:56 +0530

    Update server.mjs

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const webrtc = require("wrtc");
+import express from 'express';
+import bodyParser from 'body-parser';
+import open from 'open';
+import wrtc from 'wrtc';  // Import the entire module as default
+
+const { RTCPeerConnection, RTCSessionDescription } = wrtc;  // Destructure the needed classes
 
 let senderStream;
+
+const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/consumer", async ({ body }, res) => {
-    const peer = new webrtc.RTCPeerConnection({
+    const peer = new RTCPeerConnection({
         iceServers: [
             {
                 urls: "stun:stun.stunprotocol.org"
             }
         ]
     });
-    const desc = new webrtc.RTCSessionDescription(body.sdp);
+    const desc = new RTCSessionDescription(body.sdp);
     await peer.setRemoteDescription(desc);
     senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
     const answer = await peer.createAnswer();
@@ -30,7 +34,7 @@ app.post("/consumer", async ({ body }, res) => {
 });
 
 app.post('/broadcast', async ({ body }, res) => {
-    const peer = new webrtc.RTCPeerConnection({
+    const peer = new RTCPeerConnection({
         iceServers: [
             {
                 urls: "stun:stun.stunprotocol.org"
@@ -38,7 +42,7 @@ app.post('/broadcast', async ({ body }, res) => {
         ]
     });
     peer.ontrack = (e) => handleTrackEvent(e, peer);
-    const desc = new webrtc.RTCSessionDescription(body.sdp);
+    const desc = new RTCSessionDescription(body.sdp);
     await peer.setRemoteDescription(desc);
     const answer = await peer.createAnswer();
     await peer.setLocalDescription(answer);
@@ -53,5 +57,9 @@ function handleTrackEvent(e, peer) {
     senderStream = e.streams[0];
 };
 
-
-app.listen(5000, () => console.log('server started'));
+// Start the server and open the browser window
+app.listen(5000, () => {
+    console.log(`Server started at http://localhost:5000`);
+    // Open the browser automatically
+    open(`http://localhost:5000`);
+});
